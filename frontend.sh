2adc2a459eed1ecd9cc60ccc3b742c0b1b44c10c
satyareddy10 <sattireddy1997@gmail.com>
2023-11-04 21:10:02 +0530

    comit

--- frontend.sh
+++ frontend.sh
@@ -1,12 +1,24 @@
+echo -e "\e[36m install nginx server \e[0m"
 dnf install nginx -y
+
+echo -e "\e[36m copy file to expense conf \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf
+
+echo -e "\e[36m enable nginx server \e[0m"
 systemctl enable nginx
+
+echo -e "\e[36m start nginx server \e[0m"
 systemctl start nginx
 
+echo -e "\e[36m remove old html files in nginx server \e[0m"
 rm -rf /usr/share/nginx/html/*
 
+echo -e "\e[36m downloading frontend zip file \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip
 
+echo -e "\e[36m unzip the frontend file \e[0m"
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
+
+echo -e "\e[36m restart nginx server \e[0m"
 systemctl restart nginx
