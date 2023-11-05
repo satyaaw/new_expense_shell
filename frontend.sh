604867ad4b5a1a11983d5c88eccb4868a27c6699
satyareddy10 <sattireddy1997@gmail.com>
2023-11-05 10:22:29 +0530

    comit

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,6 @@
 echo -e "\e[36m install nginx server \e[0m"
 dnf install nginx -y  >/tmp/expense.log
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
@@ -8,7 +8,7 @@ fi
 
 echo -e "\e[36m copy file to expense conf \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf >/tmp/expense.log
-if [$? -eq 0]; then
+if  [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
@@ -16,7 +16,7 @@ fi
 
 echo -e "\e[36m enable nginx server \e[0m"
 systemctl enable nginx >/tmp/expense.log
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
@@ -24,7 +24,7 @@ fi
 
 echo -e "\e[36m start nginx server \e[0m"
 systemctl start nginx >/tmp/expense.log
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
@@ -32,7 +32,7 @@ fi
 
 echo -e "\e[36m remove old html files in nginx server \e[0m"
 rm -rf /usr/share/nginx/html/* >/tmp/expense.log
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
@@ -40,7 +40,7 @@ fi
 
 echo -e "\e[36m downloading frontend zip file \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>/tmp/expense.log
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
@@ -49,7 +49,7 @@ fi
 echo -e "\e[36m unzip the frontend file \e[0m"
 cd /usr/share/nginx/html >/tmp/expense.log
 unzip /tmp/frontend.zip >/tmp/expense.log
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
@@ -57,7 +57,7 @@ fi
 
 echo -e "\e[36m restart nginx server \e[0m"
 systemctl restart nginx >/tmp/expense.log
-if [$? -eq 0]; then
+if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
