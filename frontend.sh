210445c08de6e46f83558d8c79140c7b35d172ca
satyareddy10 <sattireddy1997@gmail.com>
2023-11-05 10:26:24 +0530

    comit

--- frontend.sh
+++ frontend.sh
@@ -1,5 +1,6 @@
+log_file = /tmp/expense.log
 echo -e "\e[36m install nginx server \e[0m"
-dnf install nginx -y  >/tmp/expense.log
+dnf install nginx -y  >log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
@@ -7,7 +8,7 @@ else
 fi
 
 echo -e "\e[36m copy file to expense conf \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf >/tmp/expense.log
+cp expense.conf /etc/nginx/default.d/expense.conf >log_file
 if  [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
@@ -15,7 +16,7 @@ else
 fi
 
 echo -e "\e[36m enable nginx server \e[0m"
-systemctl enable nginx >/tmp/expense.log
+systemctl enable nginx >log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
@@ -23,7 +24,7 @@ else
 fi
 
 echo -e "\e[36m start nginx server \e[0m"
-systemctl start nginx >/tmp/expense.log
+systemctl start nginx >log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
@@ -31,15 +32,14 @@ else
 fi
 
 echo -e "\e[36m remove old html files in nginx server \e[0m"
-rm -rf /usr/share/nginx/html/* >/tmp/expense.log
-if [ $? -eq 0 ]; then
+rm -rf /usr/share/nginx/html/* >log_file
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
 fi
 
 echo -e "\e[36m downloading frontend zip file \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>/tmp/expense.log
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
@@ -47,8 +47,8 @@ else
 fi
 
 echo -e "\e[36m unzip the frontend file \e[0m"
-cd /usr/share/nginx/html >/tmp/expense.log
-unzip /tmp/frontend.zip >/tmp/expense.log
+cd /usr/share/nginx/html >log_file
+unzip /tmp/frontend.zip >log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
@@ -56,7 +56,7 @@ else
 fi
 
 echo -e "\e[36m restart nginx server \e[0m"
-systemctl restart nginx >/tmp/expense.log
+systemctl restart nginx >log_file
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
