707876882633f5fbe73a293705c5821a88a9d6a2
satyareddy10 <sattireddy1997@gmail.com>
2023-11-05 10:27:56 +0530

    comit

--- frontend.sh
+++ frontend.sh
@@ -1,6 +1,6 @@
-log_file = /tmp/expense.log
+log_file=/tmp/expense.log
 echo -e "\e[36m install nginx server \e[0m"
-dnf install nginx -y  >log_file
+dnf install nginx -y  >"$log_file"
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
@@ -8,7 +8,7 @@ else
 fi
 
 echo -e "\e[36m copy file to expense conf \e[0m"
-cp expense.conf /etc/nginx/default.d/expense.conf >log_file
+cp expense.conf /etc/nginx/default.d/expense.conf >"$log_file"
 if  [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
@@ -16,7 +16,7 @@ else
 fi
 
 echo -e "\e[36m enable nginx server \e[0m"
-systemctl enable nginx >log_file
+systemctl enable nginx >"$log_file"
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
@@ -24,7 +24,7 @@ else
 fi
 
 echo -e "\e[36m start nginx server \e[0m"
-systemctl start nginx >log_file
+systemctl start nginx >"$log_file"
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
@@ -32,14 +32,14 @@ else
 fi
 
 echo -e "\e[36m remove old html files in nginx server \e[0m"
-rm -rf /usr/share/nginx/html/* >log_file
+rm -rf /usr/share/nginx/html/* >"$log_file"
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
 fi
 
 echo -e "\e[36m downloading frontend zip file \e[0m"
-curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>log_file
+curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>"$log_file"
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
@@ -47,8 +47,8 @@ else
 fi
 
 echo -e "\e[36m unzip the frontend file \e[0m"
-cd /usr/share/nginx/html >log_file
-unzip /tmp/frontend.zip >log_file
+cd /usr/share/nginx/html >"$log_file"
+unzip /tmp/frontend.zip >"$log_file"
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
@@ -56,7 +56,7 @@ else
 fi
 
 echo -e "\e[36m restart nginx server \e[0m"
-systemctl restart nginx >log_file
+systemctl restart nginx >"$log_file"
 if [ $? -eq 0 ]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
