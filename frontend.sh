6c71a06f14138b851925228fe72d701030da0e31
satyareddy10 <sattireddy1997@gmail.com>
2023-11-05 10:20:37 +0530

    comit

--- frontend.sh
+++ frontend.sh
@@ -4,6 +4,7 @@ if [$? -eq 0]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "\e[36m copy file to expense conf \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf >/tmp/expense.log
@@ -11,6 +12,7 @@ if [$? -eq 0]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "\e[36m enable nginx server \e[0m"
 systemctl enable nginx >/tmp/expense.log
@@ -18,6 +20,7 @@ if [$? -eq 0]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "\e[36m start nginx server \e[0m"
 systemctl start nginx >/tmp/expense.log
@@ -25,6 +28,7 @@ if [$? -eq 0]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "\e[36m remove old html files in nginx server \e[0m"
 rm -rf /usr/share/nginx/html/* >/tmp/expense.log
@@ -32,6 +36,7 @@ if [$? -eq 0]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "\e[36m downloading frontend zip file \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>/tmp/expense.log
@@ -39,6 +44,7 @@ if [$? -eq 0]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "\e[36m unzip the frontend file \e[0m"
 cd /usr/share/nginx/html >/tmp/expense.log
@@ -47,6 +53,7 @@ if [$? -eq 0]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
 
 echo -e "\e[36m restart nginx server \e[0m"
 systemctl restart nginx >/tmp/expense.log
@@ -54,3 +61,4 @@ if [$? -eq 0]; then
   echo -e "\e[32m SUCCESS \e[0m"
 else
   echo -e "\e[31m FAILURE \e[0m"
+fi
