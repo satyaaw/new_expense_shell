bf3e8f8c874eb41f0a2113693da9c45b9c067955
satyareddy10 <sattireddy1997@gmail.com>
2023-11-05 18:14:54 +0530

    comit

--- backend.sh
+++ backend.sh
@@ -20,4 +20,4 @@ systemctl start backend
 
 dnf install mysql -y
 
-mysql -h 172.31.33.174 -uroot -pExpenseApp@1 < /app/schema/backend.sql
+mysql -h 172.31.41.104 -uroot -pExpenseApp@1 < /app/schema/backend.sql
